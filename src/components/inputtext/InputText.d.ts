08d5aca9102e66fe83c9a5cde00331d1f9cf11ee
porlolicon <porlolicon@gmail.com>
2018-07-22 23:33:10 +0700

    add inputtext onvalidated to typescript

--- src/components/inputtext/InputText.d.ts
+++ src/components/inputtext/InputText.d.ts
@@ -2,6 +2,7 @@ import React = require("react");
 
 interface InputTextProps extends React.HTMLProps<HTMLInputElement> {
     [key: string]: any;
+    onValidated?(e: bool): void;
     validateState?: boolean;
     validateOnly?: boolean;
     onValidated?(e: boolean): void;
